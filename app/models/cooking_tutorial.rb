a4d9e74a7d1f8905aa8e6242dee7bbcfe10694b9
Chad Niewenhuis <chadn94@gmail.com>
2025-06-19 18:30:16 +0200

    App Created

---
+++ app/models/cooking_tutorial.rb
@@ -0,0 +1,2 @@
+class CookingTutorial < ApplicationRecord
+end
---
+++ db/migrate/20250619161341_create_cooking_tutorials.rb
@@ -0,0 +1,12 @@
+class CreateCookingTutorials < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cooking_tutorials do |t|
+      t.string :title
+      t.text :description
+      t.string :location
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
