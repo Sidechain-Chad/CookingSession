667247b02fd2ebefa804059d034ca3be1a60502a
Chad Niewenhuis <chadn94@gmail.com>
2025-06-22 10:18:23 +0200

    defaulted bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,6 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_booking, only: [:show, :edit, :update]
-
   # Bookings I'm attending and bookings for my tutorials
   def index
-    @my_bookings = current_user.bookings.includes(:cooking_tutorial)
-
-    @hosted_bookings = Booking.joins(:cooking_tutorial)
-                               .where(cooking_tutorials: { user_id: current_user.id })
-                               .includes(:user, :cooking_tutorial)
   end
 
   # See details of one booking (e.g. dietary requirements)
@@ -17,22 +9,10 @@ class BookingsController < ApplicationController
 
   # Booking form for a tutorial
   def new
-    @cooking_tutorial = CookingTutorial.find(params[:cooking_tutorial_id])
-    @booking = Booking.new
   end
 
   # Submit a new booking
   def create
-    @cooking_tutorial = CookingTutorial.find(params[:cooking_tutorial_id])
-    @booking = Booking.new(booking_params)
-    @booking.user = current_user
-    @booking.cooking_tutorial = @cooking_tutorial
-
-    if @booking.save
-      redirect_to bookings_path, notice: "Booking created successfully."
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   # Edit a booking (e.g. confirm or update dietary info)
@@ -41,20 +21,5 @@ class BookingsController < ApplicationController
 
   # Update a booking
   def update
-    if @booking.update(booking_params)
-      redirect_to bookings_path, notice: "Booking updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def booking_params
-    params.require(:booking).permit(:confirmed)
   end
 end
