09b000f7febc8a8efe0db446fb0cc1b128a8bed7
Chad Niewenhuis <chadn94@gmail.com>
2025-06-26 20:38:45 +0200

    show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,53 +1,49 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
-  # GET /bookings
   def index
     @bookings = current_user.bookings.order(created_at: :desc)
   end
 
-  # GET /bookings/:id
   def show
     authorize_booking!
   end
 
-  # GET /bookings/new
   def new
-    @cooking_class = CookingClass.find(params[:cooking_class_id])
-    @booking = Booking.new(cooking_class: @cooking_class)
+    @cooking_tutorial = CookingTutorial.find(params[:cooking_tutorial_id])
+    @booking = Booking.new(cooking_tutorial: @cooking_tutorial)
   end
 
-  # POST /bookings
   def create
+    @cooking_tutorial = CookingTutorial.find(params[:cooking_tutorial_id])
     @booking = current_user.bookings.build(booking_params)
+    @booking.cooking_tutorial = @cooking_tutorial
+
     if @booking.save
-      redirect_to @booking, notice: 'Booking was successfully created.'
+      redirect_to @booking, notice: "Booking was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # GET /bookings/:id/edit
   def edit
     authorize_booking!
   end
 
-  # PATCH/PUT /bookings/:id
   def update
     authorize_booking!
     if @booking.update(booking_params)
-      redirect_to @booking, notice: 'Booking was successfully updated.'
+      redirect_to @booking, notice: "Booking was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /bookings/:id
   def destroy
     authorize_booking!
     @booking.destroy
-    redirect_to bookings_path, notice: 'Booking was successfully cancelled.'
+    redirect_to bookings_path, notice: "Booking was successfully cancelled."
   end
 
   private
@@ -57,10 +53,10 @@ class BookingsController < ApplicationController
   end
 
   def authorize_booking!
-    redirect_to bookings_path, alert: 'Access denied.' unless @booking.user == current_user
+    redirect_to bookings_path, alert: "Access denied." unless @booking.user == current_user
   end
 
   def booking_params
-    params.require(:booking).permit(:cooking_class_id, :scheduled_at, :dietary_restrictions, :status)
+    params.require(:booking).permit(:confirmed)
   end
 end
