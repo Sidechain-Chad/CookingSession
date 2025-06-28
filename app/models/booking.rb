5974864d55c92bd55807b4fba92e82acefea0f0f
Chad Niewenhuis <chadn94@gmail.com>
2025-06-28 16:27:04 +0200

    changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,8 @@
 class Booking < ApplicationRecord
-<<<<<<< HEAD
-=======
   # Associations
   belongs_to :user
   belongs_to :cooking_tutorial
   # Validations
   validates :user_id, uniqueness: { scope: :cooking_tutorial_id, message: "has already booked this tutorial" }
   validates :confirmed, inclusion: { in: [true, false] }
->>>>>>> origin/foundation-code
 end
--- app/models/cooking_tutorial.rb
+++ app/models/cooking_tutorial.rb
@@ -1,12 +1,8 @@
 class CookingTutorial < ApplicationRecord
-<<<<<<< HEAD
-=======
-    # Associations
+  # Associations
   belongs_to :user
   has_many :users, through: :bookings
   has_many :bookings
-
-    # Validations
+  # Validations
   validates :title, :description, :location, :date, presence: true
->>>>>>> origin/foundation-code
 end
